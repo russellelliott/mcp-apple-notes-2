4a0c654eb43de410f4727ad9bd8d1d71f26961b3
Russell Elliott <russell@quadsquad.net>
2026-01-22 19:17:46 -0800

    using more secure ip address

--- server.py
+++ server.py
@@ -224,4 +224,4 @@ async def health():
 if __name__ == "__main__":
     import uvicorn
     # Use standard port 8000
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
