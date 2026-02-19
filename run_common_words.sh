af5c388d90cbee7adaca5f367b21d9f43335392b
Russell Elliott <russell@quadsquad.net>
2026-02-18 18:40:53 -0800

    get common words easily

---
+++ run_common_words.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Store the root directory of the script
+ROOT_DIR="$(dirname "$(realpath "$0")")"
+
+echo "Calculating common words..."
+
+# Add the root directory to PYTHONPATH so python can find the 'backend' module
+export PYTHONPATH="$ROOT_DIR:$PYTHONPATH"
+
+# Run backend/scripts/common_words.py
+python3 "$ROOT_DIR/backend/scripts/common_words.py" 
