11384faee2cade850ab27784249bcb13eddf6b23
Russell Elliott <russell@quadsquad.net>
2026-02-21 15:36:18 -0800

    figuring out stop words

--- run_common_words.sh
+++ run_common_words.sh
@@ -10,4 +10,4 @@ echo "Calculating common words..."
 export PYTHONPATH="$ROOT_DIR:$PYTHONPATH"
 
 # Run backend/scripts/common_words.py
-python3 "$ROOT_DIR/backend/scripts/common_words.py" 
+python3 "$ROOT_DIR/backend/scripts/common_words.py" -n 100 # Words with frequency >0.10% have a histogram bar. use those.
